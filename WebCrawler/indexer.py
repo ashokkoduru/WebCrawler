cd3957f2dcd739160f9232b97086b944605aa6c0
Ashok Koduru <ashokvsk@ccs.neu.edu>
2016-11-06 01:15:54 -0400

    Indexer started

--- WebCrawler/indexer.py
+++ WebCrawler/indexer.py
@@ -27,60 +27,84 @@ class Indexer:
             os.makedirs(parsed_corpus, 0755)
 
         i = 1
-        for link in links:
-            print i, links.index(link)+1, link
-            try:
-                full_page = requests.get(link)
-            except:
-                print "Something wrong while requesting the page " + link
+        # for link in links:
+        link = links[0]
+        print i, links.index(link)+1, link
+        try:
+            full_page = requests.get(link)
+        except:
+            print "Something wrong while requesting the page " + link
 
-            full_content = BeautifulSoup(full_page.text, 'html.parser')
-            main_content = full_content.find("div", {"id": "mw-content-text"})
-            ignore_div = ['toc', 'thumb', 'navbox', 'reflist']
-            for section in ignore_div:
-                for div in main_content.find_all('div', {'class': section}):
-                    div.decompose()
-            for div in main_content.find_all('table', {'class': 'vertical-navbox'}):
+        full_content = BeautifulSoup(full_page.text, 'html.parser')
+        main_content = full_content.find("div", {"id": "mw-content-text"})
+        ignore_div = ['thumb', 'navbox', 'reflist']
+        for section in ignore_div:
+            for div in main_content.find_all('div', {'class': section}):
                 div.decompose()
-            for div in main_content.find_all('span', {'class': 'mw-editsection'}):
-                div.decompose()
-            for div in main_content.find_all('span', {'id': 'References'}):
-                div.decompose()
-            for div in main_content.find_all('sup'):
-                div.decompose()
-            parsed_content = self.parse_page(main_content.get_text().encode('utf-8'))
-            print type(parsed_content)
-            print parsed_content.split()
-            return
-            docid = link[30:]
-            docid = docid.translate(string.maketrans("", ""), string.punctuation)
-            # docid = docid.replace('/', '')
-            # docid = docid.replace('_', '')
-            # docid = docid.replace('-', '')
-            filename = "%s.txt" % docid
-            fl = open(os.path.join(corpus, filename), 'w')
-            fl.write(str(main_content))
-            fl.close()
-            parsed_fl = open(os.path.join(parsed_corpus, filename), 'w')
-            # parsed_fl.write(str(main_content))
-            parsed_fl.write(parsed_content)
-            parsed_fl.close()
-            i += 1
+        for div in main_content.find_all('table', {'class': 'vertical-navbox'}):
+            div.decompose()
+        for div in main_content.find_all('span', {'class': 'mw-editsection'}):
+            div.decompose()
+        for div in main_content.find_all('span', {'id': 'References'}):
+            div.decompose()
+        for div in main_content.find_all('sup'):
+            div.decompose()
+        parsed_content = self.parse_page(main_content.get_text().encode('utf-8'))
+
+        docid = link[30:]
+        docid = docid.translate(string.maketrans("", ""), string.punctuation)
+        filename = "%s.txt" % docid
+        fl = open(os.path.join(corpus, filename), 'w')
+        fl.write(str(main_content))
+        fl.close()
+        parsed_fl = open(os.path.join(parsed_corpus, filename), 'w')
+        # parsed_fl.write(str(main_content))
+        parsed_fl.write(parsed_content)
+        parsed_fl.close()
+        i += 1
 
     def parse_page(self, content):
+        ignore_list = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '{', '[', '}', ']', '|',
+                       '\\', '"', "'", ';', ':', '/', '<', '>', '?']
+        print content
+        content = content.translate(None, ''.join(ignore_list))
+        content = content.split()
+        print content
+        final_content = ''
+        # print content
+        for eachword in content:
 
-        ignore_list = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', '|',
-                       '\\', '"', "'", ';', ':', ',', '.', '/', '<', '>', '?']
-
-        return content.translate(string.maketrans("", ""), string.punctuation)
+            print eachword
+            eachword = eachword.strip('.,')
+            # eachword = eachword.strip(',')
+            # if comma in eachword:
+            #    if eachword.index(comma) == len(eachword)-1:
+            #        eachword.re
+            # return eachword
+            final_content += eachword + ' '
+            # print
+        # print "dfdfdf"
+        # print final_content
+        return final_content
 
 
 def hw3_tasks():
     ind = Indexer()
-    ind.download_pages()
-    # s = "string. With. Punctuation? 5.$$$8"  # Sample string
-    # out = s.translate(string.maketrans("", ""), string.punctuation)
+    # ind.download_pages()
+    s = "string. With. Punctuation? \n \n \n 5.8 wit.h 3,200"  # Sample string
+    print ind.parse_page(s)
+    # print "sdfsdf"
+    # return
+    # out = s.translate(' ', string.punctuation)
+    # out = s.strip(string.punctuation).encode('utf-8').split()
     # print os.path.join(os.path.dirname(__file__), 'abc.txt')
+    # print s, out
+    # ignore_list = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '{', '[', '}', ']', '|',
+    #                '\\', '"', "'", ';', ':', ',', '.', '/', '<', '>', '?']
+    # out = s.translate(None, ''.join(ignore_list))
+    # chars_to_remove = ['.', '!', '?']
+    # subj = 'A.B!C?'
+    # x = re.sub("[a-zA-Z].", "", s)
     # print out
     return
 
